7079d770c4984f32021a82b32b76d570131bd001
step1step2 <applesoft5@web.de>
2015-08-09 12:35:10 +0200

    update for clown

--- RageBlock/Program.cs
+++ RageBlock/Program.cs
@@ -28,43 +28,41 @@ namespace RageBlock
             #region Flame
             flame = new string[] {
 	            "bronze", "silver", "gold", "platinum", "plat", "diamond", "master", "challenger", "feed", "retard", "ks", 
-                "killsteal", "on purpose", "fu", "fail", "failed", "bad", "gank", "camped", "stupid", "anal", "anus", "arrse", 
-                "arse", "ass-fucker", "assfucker", "assfukka", "asshole", "assholes", "asswhole", "mutter", "mother", 
-                "b!tch", "b17ch", "b1tch", "ballbag", "ballsack", "bastard", "beastial", "beastiality", "scrub",
-                "bellend", "bestial", "bestiality", "bi+ch", "biatch", "bitch", "bitcher", "bitchers", "bitches", "bitchin", 
-                "bitching", "bloody", "blow job", "blowjob", "blowjobs", "boiolas", "bollock", "bollok", "boner", 
-                "buceta", "bugger", "bum", "bunny fucker", "*", "hure", "lappen", "dumm", "dämlich", "missgeburt",
-                "butthole", "buttmuch", "buttplug", "c0ck", "c0cksucker", "carpet muncher", "cawk", "chink", "cipa", 
-                "cl1t", "clit", "clitoris", "clits", "cnut", "cock", "cock-sucker", "cockface", "cockhead", "cockmunch", 
-                "cockmuncher", "cocks", "cocksuck", "cocksucked", "cocksucker", "cocksucking", "cocksucks", "cocksuka", 
-                "cocksukka", "cok", "cokmuncher", "coksucka", "coon", "cox", "crap", "cum", "cummer", "cumming", "cums", 
-                "cumshot", "cunilingus", "cunillingus", "cunnilingus", "cunt", "cuntlick", "cuntlicker", "cuntlicking", 
-                "cunts", "cyalis", "cyberfuc", "cyberfuck", "cyberfucked", "cyberfucker", "cyberfuckers", "cyberfucking", 
-                "d1ck", "damn", "dick", "dickhead", "dildo", "dildos", "dink", "dinks", "dirsa", "dlck", "dog-fucker", 
-                "doggin", "dogging", "donkeyribber", "doosh", "duche", "dyke", "ejaculate", "ejaculated", "ejaculates", 
-                "ejaculating", "ejaculatings", "ejaculation", "ejakulate", "f u c k", "f u c k e r", "f4nny", "fag", 
-                "fagging", "faggitt", "faggot", "faggs", "fagot", "fagots", "fags", "fanny", "fannyflaps", "fannyfucker", 
-                "fanyy", "fatass", "fcuk", "fcuker", "fcuking", "feck", "fecker", "felching", "fellate", "fellatio", 
-                "fingerfuck", "fingerfucked", "fingerfucker", "fingerfuckers", "fingerfucking", "fingerfucks", "fistfuck", 
-                "fistfucked", "fistfucker", "fistfuckers", "fistfucking", "fistfuckings", "fistfucks", "flange", "fook", 
-                "fooker", "fuck", "fucka", "fucked", "fucker", "fuckers", "fuckhead", "fuckheads", "fuckin", "fucking", 
-                "fuckings", "fuckingshitmotherfucker", "fuckme", "fucks", "fuckwhit", "fuckwit", "fudge packer", "ass",
-                "fudgepacker", "fuk", "fuker", "fukker", "fukkin", "fuks", "fukwhit", "fukwit", "fux", "fux0r", "f_u_c_k", 
-                "gangbang", "gangbanged", "gangbangs", "gaylord", "gaysex", "goatse", "God", "god-dam", "god-damned", 
-                "goddamn", "goddamned", "hardcoresex", "hell", "heshe", "hoar", "hoare", "hoer", "homo", "hore", "fick",
-                "horniest", "horny", "hotsex", "jack-off", "jackoff", "jerk-off", "jism", "jiz", "jizm", "jizz", 
-                "kawk", "knob", "knobead", "knobed", "knobend", "knobhead", "knobjocky", "knobjokey", "kock", "kondum", 
-                "kondums", "kum", "kummer", "kumming", "kums", "kunilingus", "l3i+ch", "l3itch", "labia", "lmfao", 
-                "lusting", "m0f0", "m0fo", "m45terbate", "ma5terb8", "ma5terbate", "masochist", "master-bate", "useless",
-                "masterb8", "masterbat*", "masterbat3", "masterbate", "masterbation", "masterbations", "masturbate", 
-                "mo-fo", "mof0", "mofo", "mothafuck", "mothafucka", "mothafuckas", "mothafuckaz", "mothafucked", 
-                "mothafucker", "mothafuckers", "mothafuckin", "mothafucking", "mothafuckings", "mothafucks", "mother fucker", 
-                "motherfuck", "motherfucked", "motherfucker", "motherfuckers", "motherfuckin", "motherfucking", "stupid",
-                "motherfuckings", "motherfuckka", "motherfucks", "muff", "mutha", "muthafecker", "muthafuckker", "muther", 
-                "mutherfucker", "n1gga", "n1gger", "nazi", "nigg3r", "nigg4h", "nigga", "niggah", "niggas", "niggaz", 
-                "nigger", "niggers", "nob", "nob jokey", "nobhead", "nobjocky", "nobjokey", "numbnuts", "nutsack", "***",
-                "orgasim", "orgasims", "orgasm", "orgasms", "p0rn", "pawn", "pecker", "penis", "penisfucker", "phonesex", 
-                "phuck", "phuk", "phuked", "phuking", "phukked", "phukking", "phuks", "phuq", "pigfucker", "pimpis", 
+                "killsteal", "on purpose", "camped", "stupid", "anal", "anus", "arrse", "arse", "ass-fucker", "assfucker", 
+                "assfukka", "asshole", "assholes", "asswhole", "mutter", "mother", "b!tch", "b17ch", "b1tch", "ballbag", 
+                "ballsack", "bastard", "beastial", "beastiality", "scrub", "bellend", "bestial", "bestiality", "bi+ch", 
+                "biatch", "bitch", "bitcher", "bitchers", "bitches", "bitchin", "bitching", "bloody", "blow job", "blowjob",
+                "blowjobs", "boiolas", "bollock", "bollok", "boner", "buceta", "bugger", "bum", "bunny fucker", "hure", 
+                "lappen", "dumm", "dämlich", "missgeburt", "butthole", "buttmuch", "buttplug", "c0ck", "c0cksucker", 
+                "carpet muncher", "cawk", "chink", "cipa", "cl1t", "clit", "clitoris", "clits", "cnut", "cock", "cock-sucker", 
+                "cockface", "cockhead", "cockmunch", "cockmuncher", "cocks", "cocksuck", "cocksucked", "cocksucker", 
+                "cocksucking", "cocksucks", "cocksuka", "cocksukka", "cok", "cokmuncher", "coksucka", "coon", "cox", "crap", 
+                "cum", "cummer", "cumming", "cums", "cumshot", "cunilingus", "cunillingus", "cunnilingus", "cunt", "cuntlick", 
+                "cuntlicker", "cuntlicking", "cunts", "cyalis", "cyberfuc", "cyberfuck", "cyberfucked", "cyberfucker", 
+                "cyberfuckers", "cyberfucking", "d1ck", "damn", "dick", "dickhead", "dildo", "dildos", "dink", "dinks", "dirsa", 
+                "dlck", "dog-fucker", "doggin", "dogging", "donkeyribber", "doosh", "duche", "dyke", "ejaculate", "ejaculated", 
+                "ejaculates", "ejaculating", "ejaculatings", "ejaculation", "ejakulate", "f u c k", "f u c k e r", "f4nny", "fag", 
+                "fagging", "faggitt", "faggot", "faggs", "fagot", "fagots", "fags", "fanny", "fannyflaps", "fannyfucker", "fanyy",
+                "fatass", "fcuk", "fcuker", "fcuking", "feck", "fecker", "felching", "fellate", "fellatio", "fingerfuck", 
+                "fingerfucked", "fingerfucker", "fingerfuckers", "fingerfucking", "fingerfucks", "fistfuck", "fistfucked", 
+                "fistfucker", "fistfuckers", "fistfucking", "fistfuckings", "fistfucks", "flange", "fook", "fooker", "fuck", 
+                "fucka", "fucked", "fucker", "fuckers", "fuckhead", "fuckheads", "fuckin", "fucking", "fuckings", "script",
+                "fuckingshitmotherfucker", "fuckme", "fucks", "fuckwhit", "fuckwit", "fudge packer", "ass", "fudgepacker", 
+                "fuk", "fuker", "fukker", "fukkin", "fuks", "fukwhit", "fukwit", "fux", "fux0r", "f_u_c_k", "gangbang", 
+                "gangbanged", "gangbangs", "gaylord", "gaysex", "goatse", "God", "god-dam", "god-damned", "goddamn", "goddamned", 
+                "hardcoresex", "hell", "heshe", "hoar", "hoare", "hoer", "homo", "hore", "fick", "horniest", "horny", "hotsex", 
+                "jack-off", "jackoff", "jerk-off", "jism", "jiz", "jizm", "jizz", "kawk", "knob", "knobead", "knobed", "knobend", 
+                "knobhead", "knobjocky", "knobjokey", "kock", "kondum", "kondums", "kum", "kummer", "kumming", "kums", 
+                "kunilingus", "l3i+ch", "l3itch", "labia", "lmfao", "lusting", "m0f0", "m0fo", "m45terbate", "ma5terb8", 
+                "ma5terbate", "masochist", "master-bate", "useless", "masterb8", "masterbat*", "masterbat3", "masterbate", 
+                "masterbation", "masterbations", "masturbate", "mo-fo", "mof0", "mofo", "mothafuck", "mothafucka", "mothafuckas",
+                "mothafuckaz", "mothafucked", "mothafucker", "mothafuckers", "mothafuckin", "mothafucking", "mothafuckings", 
+                "mothafucks", "mother fucker", "motherfuck", "motherfucked", "motherfucker", "motherfuckers", "motherfuckin", 
+                "motherfucking", "stupid", "motherfuckings", "motherfuckka", "motherfucks", "muff", "mutha", "muthafecker", 
+                "muthafuckker", "muther", "mutherfucker", "n1gga", "n1gger", "nazi", "nigg3r", "nigg4h", "nigga", "niggah",
+                "niggas", "niggaz", "nigger", "niggers", "nob", "nob jokey", "nobhead", "nobjocky", "nobjokey", "numbnuts", 
+                "nutsack", "***", "orgasim", "orgasims", "orgasm", "orgasms", "p0rn", "pawn", "pecker", "penis", "penisfucker", 
+                "phonesex", "phuck", "phuk", "phuked", "phuking", "phukked", "phukking", "phuks", "phuq", "pigfucker", "pimpis", 
                 "piss", "pissed", "pisser", "pissers", "pisses", "pissflaps", "pissin", "pissing", "pissoff", "poop", 
                 "porn", "porno", "pornography", "pornos", "prick", "pricks", "pron", "pube", "pusse", "pussi", "pussies", 
                 "pussy", "pussys", "rectum", "rimjaw", "rimming", "s hit", "s.o.b.", "sadist", "schlong", "screwing", 
@@ -78,10 +76,28 @@ namespace RageBlock
                 "wanky", "whoar", "whore", "willies", "willy", "xrated", "xxx", "noob", "nap", "suck", "fuck", "report"
             };
             #endregion
+            #region Jokes
             neverflame = new string[] {
-                "You should not flame sweety", "Gotcha, next time maybe.", "Hey! You know why, do you?", "You want skins do you?"
+                //"You should not flame sweety.", "Gotcha, next time maybe.", "Stop it.", "You want skins do you?", 
+                "Trundle never ceases to be amazed about how everyone on the internet knows he's a troll.",
+                "Mordekaiser really hates airport security.", 
+                "Shacos darkest secret is that he's afraid of children.",
+                "You know Zilean is having a bad day when he asks you for the time.", 
+                "Once Kog'maw had a cold, but no one could tell.", 
+                "Also, Evelynn is constantly being bothered by an old man named Gargamel.", 
+                "For some reason Alistar suffers from headaches.", 
+                "Ryze's big secret is a real shocker!",
+                "Cassiopeia always knows how much she weighs thanks to all her scales.",
+                "Taric's employer thinks he's a real gem.", "Sheep are suspicious of the newcomer: Urf the manatee",
+                "Sometimes Karthus picks up a sword and pretends that he's Arthas.", 
+                "Tristana is disappointed that it's never a rocket in your pocket.",
+                "Why Mundo can't be jailed for sexual assault? Mundo pleases who ever he pleases!",
+                "Yorick walked into a bar. There was no counter.",
+                "Why didn't Sivir win the spelling bee? Because she could only spell shield",
+                "Why did Fizz fall off of his trident? Because he's incredibly unbalanced."
             };
-            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+            #endregion
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad; 
         }
 
         private static void Game_OnGameLoad(EventArgs args)
@@ -96,29 +112,51 @@ namespace RageBlock
             #endregion
             Game.OnUpdate += Game_OnUpdate;
             Game.OnChat += Game_OnChat;
-            Log("has been loaded.");
+            Game.OnInput += Game_OnInput;
         }
 
         public static String GetTimestamp(DateTime value) { return value.ToString("HH:mm"); }
 
-        private static void Log(string Value) { Console.WriteLine("[" + timeStamp + "] RageBlock " + Value); }
+        private static void Log(string Value) { 
+            Console.WriteLine("[" + timeStamp + "] RageBlock " + Value);
+            Game.PrintChat("[" + timeStamp + "] <font color='#eb7577'>RageBlock</font>: " + Value);
+        }
 
         private static void Game_OnChat(GameChatEventArgs args)
         {
             if (!M.Item("Status").GetValue<bool>()) return;
             var someAdvice = neverflame[new Random().Next(0, neverflame.Length)];
-            if (args.Sender.IsMe)
+
+            if (!args.Sender.IsMe)
             {
                 foreach (string item in args.Message.Split(' '))
+                {
+                    if (!muted.Any(str => str.Contains(args.Sender.Name)) && flame.Any(item.Contains))
+                    {
+                        muted.Add(args.Sender.Name);
+                        Utility.DelayAction.Add(new Random().Next(127, 723), () => Game.Say("/mute " + args.Sender.Name));                        
+                        //Log(args.Sender.ChampionName + " has been muted");
+                        Notifications
+                            .AddNotification(new Notification(args.Sender.ChampionName + " has been muted.", 3500)
+                            .SetTextColor(Color.OrangeRed)
+                            .SetBoxColor(Color.Black));
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void Game_OnInput(GameInputEventArgs args)
+        {
+            if (!M.Item("Status").GetValue<bool>()) return;
+            var someAdvice = neverflame[new Random().Next(0, neverflame.Length)];
+
+                foreach (string item in args.Input.Split(' '))
                 {
                     if (flame.Any(item.Contains))
                     {
                         args.Process = false;
                         Log(someAdvice);
-                        Notifications
-                            .AddNotification(new Notification(someAdvice, 3500)
-                            .SetTextColor(Color.OrangeRed)
-                            .SetBoxColor(Color.Black));
                         break;
                     }
                 }
@@ -132,24 +170,6 @@ namespace RageBlock
                 //        .SetTextColor(Color.OrangeRed)
                 //        .SetBoxColor(Color.Black));
                 //}
-            }
-            if (!args.Sender.IsMe)
-            {
-                foreach (string item in args.Message.Split(' '))
-                {
-                    if (!muted.Any(str => str.Contains(args.Sender.Name)) && flame.Any(item.Contains))
-                    {
-                        muted.Add(args.Sender.Name);
-                        Utility.DelayAction.Add(new Random().Next(127, 723), () => Game.Say("/mute " + args.Sender.Name));                        
-                        Log("has been muted");
-                        Notifications
-                            .AddNotification(new Notification(args.Sender.Name + " has been muted.", 3500)
-                            .SetTextColor(Color.OrangeRed)
-                            .SetBoxColor(Color.Black));
-                        break;
-                    }
-                }
-            }
         }
 
         private static void Game_OnUpdate(EventArgs args)

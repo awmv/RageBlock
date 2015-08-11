5485553596fea012e6118c0c4b3c00dd0c294eb7
step1step2 <applesoft5@web.de>
2015-08-11 20:41:19 +0200

    derp

--- RageBlock/Program.cs
+++ RageBlock/Program.cs
@@ -38,17 +38,20 @@ namespace RageBlock
 
         private static void Game_OnChat(GameChatEventArgs args)
         {
-            if (!M.Item("Status").GetValue<bool>() && !args.Sender.IsMe) return;
-            Regex regex = new Regex(string.Join("|\\b", RageBlock.Rage.flame), RegexOptions.IgnoreCase);
-            Match match = regex.Match(args.Message);
-            if (match.Success)
+            if (!M.Item("Status").GetValue<bool>()) return;
+            if (!args.Sender.IsMe)
             {
-                muted.Add(args.Sender.Name);
-                Utility.DelayAction.Add(new Random().Next(127, 723), () => Game.Say("/mute " + args.Sender.Name));
-                Notifications
-                    .AddNotification(new Notification(args.Sender.ChampionName + " has been muted.", 3500)
-                    .SetTextColor(Color.OrangeRed)
-                    .SetBoxColor(Color.Black));
+                Regex regex = new Regex(string.Join("|\\b", RageBlock.Rage.flame), RegexOptions.IgnoreCase);
+                Match match = regex.Match(args.Message);
+                if (match.Success)
+                {
+                    muted.Add(args.Sender.Name);
+                    Utility.DelayAction.Add(new Random().Next(127, 723), () => Game.Say("/mute " + args.Sender.Name));
+                    Notifications
+                        .AddNotification(new Notification(args.Sender.ChampionName + " has been muted.", 3500)
+                        .SetTextColor(Color.OrangeRed)
+                        .SetBoxColor(Color.Black));
+                }
             }
         }
 
